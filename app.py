408775e99adc5ee98d8aedfe4f5445e311f1f05b
GayatriBodhe <gayatrimbodhe12@gmail.com>
2025-01-30 23:53:27 +0530

    flask framework structuring

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home Page (Index)
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
+
+# Login Page
+@app.route("/login")
+def login():
+    return render_template("Login.html")
+
+# Register Page
+@app.route("/register")
+def register():
+    return render_template("Register.html")
+
+# Admin Page (New)
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
